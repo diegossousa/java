32e60e870043412a07d05879594f645531a96181
Diego <diegos_sousa@hotmail.com>
2021-04-23 17:46:31 -0300

    Change Name the Main Class

--- src/main/java/one/digitalinnovation/personapi/config/DataConfigurator.java
+++ src/main/java/one/digitalinnovation/personapi/config/DataConfigurator.java
@@ -8,27 +8,56 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class DataConfigurator {
 
     @Bean
-    public DataSource getDataSource() {
+    public DataSource dataPostgreSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
         return DataSourceBuilder.create()
-                .driverClassName("com.mysql.cj.jdbc.Driver")
-                .password("82275813")
-                .url("jdbc:mysql://localhost:3306/livraria?serverTimezone=UTC")
-                .username("livraria")
+                .url(dbUrl)
+                .username(username)
+                .password(password)
                 .build();
     }
 
     @Bean
-    public JpaVendorAdapter getVendorAdapter() {
+    public JpaVendorAdapter jpaPostgreAdapter() {
         HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+
         adapter.setShowSql(true);
-        adapter.setPrepareConnection(true);
         adapter.setGenerateDdl(true);
-        adapter.setDatabase(Database.MYSQL);
+        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+        adapter.setPrepareConnection(true);
         return adapter;
     }
+
+//    @Bean
+//    public DataSource getDataSource() {
+//        return DataSourceBuilder.create()
+//                .driverClassName("com.mysql.cj.jdbc.Driver")
+//                .password("82275813")
+//                .url("jdbc:mysql://localhost:3306/livraria?serverTimezone=UTC")
+//                .username("livraria")
+//                .build();
+//    }
+//
+//    @Bean
+//    public JpaVendorAdapter getVendorAdapter() {
+//        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//        adapter.setShowSql(true);
+//        adapter.setPrepareConnection(true);
+//        adapter.setGenerateDdl(true);
+//        adapter.setDatabase(Database.MYSQL);
+//        return adapter;
+//    }
 }
